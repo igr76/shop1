ed32b1f40292d366f172abebacb7ebbb5381068e
igr76 <gritsuk766@gmail.com>
2024-05-23 21:43:43 +1000

    UserDTO    ResponseWrapperComment

---
+++ src/main/java/com/example/shop1/dto/UserDTO.java
@@ -0,0 +1,53 @@
+package com.example.shop1.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * DTO сущности
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDTO {
+
+  /**
+   * почта пользователя
+   */
+  String email;
+
+  /**
+   * Имя пользователя
+   */
+  String firstName;
+  /**
+   * id пользователя
+   */
+  Integer id;
+  /**
+   * Фамилия пользователя
+   */
+  String lastName;
+  /**
+   * телефон пользователя
+   */
+  String phone;
+  /**
+   * дата регистрации пользователя
+   */
+  String regDate;
+  /**
+   * город пользователя
+   */
+  String city;
+  /**
+   * фото пользователя
+   */
+
+  String image;
+
+}
